141b768f1aaec203a373bd12eb90b6324ab5e092
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-01-12 18:22:53 +0200

    Moved independent stuff to lib. Added glsl-prefix

---
+++ game/lib/glsl-prefix.h
@@ -0,0 +1,17 @@
+// BlockOut 3000
+// Copyright(c) 2017-2021 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// http://opensource.org/licenses/MIT
+//
+#pragma once
+
+#if defined(__EMSCRIPTEN__)
+#   define GLSL_VERT_PREFIX "#version 300 es\n"
+#   define GLSL_FRAG_PREFIX "#version 300 es\n" \
+                            "precision mediump float;\n"
+#else
+#   define GLSL_VERT_PREFIX "#version 330\n"
+#   define GLSL_FRAG_PREFIX "#version 330\n"
+#endif
