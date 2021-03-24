79131058aaec8d5b5e6e1bb64608cad08f4cc496
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-03-24 17:23:04 +0200

    Block builder layout WiP

---
+++ game/LayoutBlockBuilder.cpp
@@ -0,0 +1,52 @@
+// BlockOut 3000
+// Copyright(c) 2017-2021 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// http://opensource.org/licenses/MIT
+//
+#include "LayoutBlockBuilder.hpp"
+
+void LayoutBlockBuilder::updateElements()
+{
+    if (m_namedElements.empty()) createElements();
+}
+
+void LayoutBlockBuilder::createElements()
+{
+    set_contain(m_root, LAY_ROW);
+
+    {
+        m_leftColumn.name = "ColLeft";
+        auto id = item();
+        set_size_xy(id, 300, 0);
+        set_behave(id, LAY_VFILL);
+        insert(m_root, id);
+        m_leftColumn.id = id;
+        registerNamedElement(m_leftColumn);
+    }
+
+    auto right = item();
+    set_behave(right, LAY_HFILL | LAY_VFILL);
+    set_contain(right, LAY_COLUMN);
+    insert(m_root, right);
+
+    {
+        m_right1.name = "Right A";
+        auto id = item();
+        set_size_xy(id, 0, 330);
+        set_behave(id, LAY_HFILL);
+        insert(right, id);
+        m_right1.id = id;
+        registerNamedElement(m_right1);
+    }
+
+    {
+        m_right2.name = "Right B";
+        auto id = item();
+        set_behave(id, LAY_HFILL | LAY_VFILL);
+        insert(right, id);
+        m_right2.id = id;
+        registerNamedElement(m_right2);
+    }
+}
--- game/ModeLayoutTest.cpp
+++ game/ModeLayoutTest.cpp
@@ -11,6 +11,7 @@
 #include "AppMode.hpp"
 
 #include "LayoutExperimental.hpp"
+#include "LayoutBlockBuilder.hpp"
 
 #include "lib/imgui.hpp"
 #include "lib/sokol-app.h"
@@ -23,23 +24,32 @@ public:
     ModeLayoutTest()
     {
         // add layouts to test with
+        m_layouts.emplace_back(new LayoutBlockBuilder);
         m_layouts.emplace_back(new LayoutExperimental);
-
-        m_curLayout = m_layouts.front().get();
     }
 
     virtual const char* name() const override { return "Layout Test"; }
 
     virtual void defaultRender(ivec2 windowSize) override
     {
-        m_curLayout->update(windowSize);
-        for (auto& elem : m_curLayout->namedElements())
+        auto cur = m_layouts[m_curLayoutIndex].get();
+        cur->update(windowSize);
+        for (auto& elem : cur->namedElements())
         {
             ImGui::SetNextWindowPos({float(elem->topLeft.x), float(elem->topLeft.y)});
             ImGui::SetNextWindowSize({float(elem->size.x), float(elem->size.y)});
             ImGui::Begin(elem->name.c_str());
             ImGui::End();
         }
+
+        ImGui::Begin("Selector");
+        auto selector = [](void* pdata, int index, const char** out) {
+            auto& data = *static_cast<std::vector<std::unique_ptr<GUILayout>>*>(pdata);
+            *out = data[index]->name();
+            return true;
+        };
+        ImGui::ListBox("Layouts", &m_curLayoutIndex, selector, &m_layouts, int(m_layouts.size()), 10);
+        ImGui::End();
     }
 
     virtual bool handleEvent(const sapp_event& event) override
@@ -53,7 +63,7 @@ public:
     }
 
     std::vector<std::unique_ptr<GUILayout>> m_layouts;
-    GUILayout* m_curLayout;
+    int m_curLayoutIndex = 0;
 };
 } // namespace
 
