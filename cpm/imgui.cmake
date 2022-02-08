4cb47b7704197666b58b3fb1f35e4ba6eaaea18e
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-02-08 15:39:02 +0200

    Latest imgui

--- cpm/imgui.cmake
+++ cpm/imgui.cmake
@@ -1,14 +1,12 @@
-CPMAddPackage(
-    NAME imgui
-    GITHUB_REPOSITORY ocornut/imgui
-    VERSION 1.79
-)
+CPMAddPackage(gh:ocornut/imgui@1.87)
 
 add_library(imgui STATIC
     ${imgui_SOURCE_DIR}/imgui.cpp
     ${imgui_SOURCE_DIR}/imgui_demo.cpp
     ${imgui_SOURCE_DIR}/imgui_draw.cpp
-    ${imgui_SOURCE_DIR}/imgui_widgets.cpp)
+    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+)
 target_include_directories(imgui INTERFACE ${imgui_SOURCE_DIR})
 target_compile_definitions(imgui PUBLIC -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS)
 set_target_properties(imgui PROPERTIES FOLDER third-party)
