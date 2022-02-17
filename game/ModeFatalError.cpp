2c47770dec229d9334450535dc50c6dfe94a0d16
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-02-17 08:49:34 +0200

    Copyright years

--- game/ModeFatalError.cpp
+++ game/ModeFatalError.cpp
@@ -1,5 +1,5 @@
 // BlockOut 3000
-// Copyright(c) 2017-2021 Borislav Stanimirov
+// Copyright(c) 2017-2022 Borislav Stanimirov
 //
 // Distributed under the MIT Software License
 // See accompanying file LICENSE.txt or copy at
--- game/lib/Mesh.hpp
+++ game/lib/Mesh.hpp
@@ -1,3 +1,10 @@
+// BlockOut 3000
+// Copyright(c) 2017-2022 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// http://opensource.org/licenses/MIT
+//
 #pragma once
 
 #include "sokol-gfx.h"
